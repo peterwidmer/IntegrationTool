d2a69f6c21383ef4a4c30d88b8d95fc813284757
peterwidmer <peter.widmer@mail.com>
2015-05-22 20:57:57 +0200

    Begin working on Dynamics Crm Markting List Target
    MySql target is on hold for the moment. If you like to implement it. Plz contact me.

---
+++ IntegrationTool.Module.WriteDynamicCrmMarketingLists/ConfigurationWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using IntegrationTool.SDK.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntegrationTool.Module.WriteDynamicCrmMarketingLists
+{
+    /// <summary>
+    /// Interaction logic for ConfigurationWindow.xaml
+    /// </summary>
+    public partial class ConfigurationWindow : UserControl
+    {
+        private IDatastore dataObject;
+        private WriteToDynamicsCrmMarketingListsConfiguration configuration;
+
+        public ConfigurationWindow(WriteToDynamicsCrmMarketingListsConfiguration configuration, IDatastore dataObject)
+        {
+            InitializeComponent();
+            this.dataObject = dataObject;
+            this.configuration = configuration;
+        }
+    }
+}
---
+++ IntegrationTool.Module.WriteDynamicCrmMarketingLists/WriteToDynamicsCrmMarketingLists.cs
@@ -0,0 +1,39 @@
+﻿using IntegrationTool.SDK;
+using IntegrationTool.SDK.Database;
+using Microsoft.Xrm.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTool.Module.WriteDynamicCrmMarketingLists
+{
+    [TargetModuleAttribute(Name = "WriteToDynamicsCrmMarketingLists",
+                           DisplayName = "CRM Marketing-List",
+                           ModuleType = ModuleType.Target,
+                           GroupName = ModuleGroup.Target,
+                           ConnectionType = typeof(CrmConnection),
+                           ConfigurationType = typeof(WriteToDynamicsCrmMarketingListsConfiguration))]
+    public class WriteToDynamicsCrmMarketingLists : IModule, IDataTarget
+    {
+
+        public WriteToDynamicsCrmMarketingListsConfiguration Configuration { get; set; }
+
+        public System.Windows.Controls.UserControl RenderConfigurationWindow(ConfigurationBase configuration, IDatastore dataObject)
+        {
+            ConfigurationWindow configurationWindow = new ConfigurationWindow((WriteToDynamicsCrmMarketingListsConfiguration)configuration, dataObject);
+            return configurationWindow;
+        }
+
+        public void SetConfiguration(ConfigurationBase configurationBase)
+        {
+            this.Configuration = configurationBase as WriteToDynamicsCrmMarketingListsConfiguration;
+        }
+
+        public void WriteData(IConnection connection, SDK.Database.IDatabaseInterface databaseInterface, SDK.Database.IDatastore dataObject, ReportProgressMethod reportProgress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
