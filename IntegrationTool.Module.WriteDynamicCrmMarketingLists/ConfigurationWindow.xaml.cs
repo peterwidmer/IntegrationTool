94097e336a3da366271f186efc2054d574a07f04
peterwidmer <peter.widmer@mail.com>
2015-05-25 21:15:08 +0200

    Worked on marketinglist target

--- IntegrationTool.Module.WriteDynamicCrmMarketingLists/ConfigurationWindow.xaml.cs
+++ IntegrationTool.Module.WriteDynamicCrmMarketingLists/ConfigurationWindow.xaml.cs
@@ -1,6 +1,12 @@
-﻿using IntegrationTool.SDK.Database;
+﻿using IntegrationTool.SDK;
+using IntegrationTool.SDK.Database;
+using IntegrationTool.SDK.GenericControls;
+using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Client.Services;
+using Microsoft.Xrm.Sdk.Metadata;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,8 +25,11 @@ namespace IntegrationTool.Module.WriteDynamicCrmMarketingLists
     /// <summary>
     /// Interaction logic for ConfigurationWindow.xaml
     /// </summary>
-    public partial class ConfigurationWindow : UserControl
+    public partial class ConfigurationWindow : UserControl, IConnectionChanged
     {
+        private CrmConnection crmConnection;
+        private OrganizationService orgServiceInstance;
+
         private IDatastore dataObject;
         private WriteToDynamicsCrmMarketingListsConfiguration configuration;
 
@@ -47,6 +56,45 @@ namespace IntegrationTool.Module.WriteDynamicCrmMarketingLists
                     ListMappingContent.Content = new UserControls.ListMappingJoin();
                     break;
             }
+        }
+
+        public void ConnectionChanged(IConnection connection)
+        {
+            ListMappingContent.Content = new LoadingControl();
+            MemberMappingContent.Content = new LoadingControl();
+
+            this.crmConnection = connection.GetConnection() as CrmConnection;
+            this.orgServiceInstance = new OrganizationService(crmConnection);
+
+            BackgroundWorker bgwConnectionChanged = new BackgroundWorker();
+            bgwConnectionChanged.DoWork += bgwConnectionChanged_DoWork;
+            bgwConnectionChanged.RunWorkerCompleted += bgwConnectionChanged_RunWorkerCompleted;
+            bgwConnectionChanged.RunWorkerAsync();      
+        }        
+
+        void bgwConnectionChanged_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List<EntityMetadata> entitiesMetadata = new List<EntityMetadata>();
+            
+            var marketingListMetadata = Crm2013Wrapper.Crm2013Wrapper.GetEntityMetadata(this.orgServiceInstance, "marketinglist");
+            entitiesMetadata.Add(marketingListMetadata);
+
+            var accountMetadata = Crm2013Wrapper.Crm2013Wrapper.GetEntityMetadata(this.orgServiceInstance, "account");
+            entitiesMetadata.Add(accountMetadata);
+
+            var contactMetadata = Crm2013Wrapper.Crm2013Wrapper.GetEntityMetadata(this.orgServiceInstance, "contact");
+            entitiesMetadata.Add(contactMetadata);
+
+            var leadMetadata = Crm2013Wrapper.Crm2013Wrapper.GetEntityMetadata(this.orgServiceInstance, "lead");
+            entitiesMetadata.Add(leadMetadata);
+
+            e.Result = new object[] { entitiesMetadata };
+        }
+
+        void bgwConnectionChanged_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ddJoinType.IsEnabled = true;
+            ddJoinType.IsEnabled = true;
         }
     }
 }
