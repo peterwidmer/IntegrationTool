035342b096b9dd100c7b225c802df6796234e04a
peterwidmer <peter.widmer@mail.com>
2015-06-14 20:28:17 +0200

    Begin with Crm deletion target

---
+++ IntegrationTool.Module.DeleteInDynamicsCrm/DeleteInDynamicsCrm.cs
@@ -0,0 +1,39 @@
+﻿using IntegrationTool.SDK;
+using IntegrationTool.SDK.Database;
+using Microsoft.Xrm.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTool.Module.DeleteInDynamicsCrm
+{
+        [TargetModuleAttribute(Name = "DeleteInDynamicsCrm",
+                           DisplayName = "Delete CRM records",
+                           ModuleType = ModuleType.Target,
+                           GroupName = ModuleGroup.Target,
+                           ConnectionType = typeof(CrmConnection),
+                           ConfigurationType = typeof(DeleteInDynamicsCrmConfiguration))]
+    public class DeleteInDynamicsCrm : IModule, IDataTarget
+    {
+
+        public DeleteInDynamicsCrmConfiguration Configuration { get; set; }
+
+        public System.Windows.Controls.UserControl RenderConfigurationWindow(ConfigurationBase configuration, IDatastore dataObject)
+        {
+            ConfigurationWindow configurationWindow = new ConfigurationWindow((DeleteInDynamicsCrmConfiguration)configuration, dataObject);
+            return configurationWindow;
+        }
+
+        public void SetConfiguration(ConfigurationBase configurationBase)
+        {
+            this.Configuration = configurationBase as DeleteInDynamicsCrmConfiguration;
+        }
+
+        public void WriteData(IConnection connection, IDatabaseInterface databaseInterface, IDatastore dataObject, ReportProgressMethod reportProgress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
