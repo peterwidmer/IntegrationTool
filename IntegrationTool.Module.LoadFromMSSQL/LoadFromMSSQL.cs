6bcf332b8aedd135e4169b193ea2e524a4c69bf9
peterwidmer <peter.widmer@mail.com>
2015-08-07 22:08:39 +0200

    Corrected disposing of SqlCommand and SqlDataReader.

--- IntegrationTool.Module.LoadFromMSSQL/LoadFromMSSQL.cs
+++ IntegrationTool.Module.LoadFromMSSQL/LoadFromMSSQL.cs
@@ -32,43 +32,44 @@ namespace IntegrationTool.Module.LoadFromMSSQL
 
         public void LoadData(IConnection connection, IDatastore datastore, ReportProgressMethod reportProgress)
         {
-            SqlConnection sqlConnection = connection.GetConnection() as SqlConnection;
-            if(sqlConnection.State != System.Data.ConnectionState.Open)
+            using (SqlConnection sqlConnection = connection.GetConnection() as SqlConnection)
             {
                 sqlConnection.Open();
-            }
 
-            SqlCommand sqlCommand = new SqlCommand(Configuration.SqlValue, sqlConnection);
-            SqlDataReader reader = sqlCommand.ExecuteReader();
-
-            int counter = 0;
-            while(reader.Read())
-            {
-                // Read the metadata from the source
-                if(datastore.Count == 0)
+                using (SqlCommand sqlCommand = new SqlCommand(Configuration.SqlValue, sqlConnection))
                 {
-                    for (int i = 0; i < reader.FieldCount; i++)
+                    using (SqlDataReader reader = sqlCommand.ExecuteReader())
                     {
-                        datastore.AddColumnMetadata(new ColumnMetadata(i, reader.GetName(i)));
+                        int counter = 0;
+                        while (reader.Read())
+                        {
+                            // Read the metadata from the source
+                            if (datastore.Count == 0)
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    datastore.AddColumnMetadata(new ColumnMetadata(i, reader.GetName(i)));
+                                }
+                            }
+
+                            // Add data to the datastore
+                            object[] data = new object[reader.FieldCount];
+                            for (int dataIndex = 0; dataIndex < reader.FieldCount; dataIndex++)
+                            {
+                                data[dataIndex] = reader.GetValue(dataIndex);
+                            }
+                            datastore.AddData(data);
+
+                            if (StatusHelper.MustShowProgress(counter, -1) == true)
+                            {
+                                reportProgress(new SimpleProgressReport("Loaded " + (counter + 1) + " records"));
+                            }
+
+                            counter++;
+                        }
                     }
                 }
-
-                // Add data to the datastore
-                object[] data = new object[reader.FieldCount];
-                for(int dataIndex = 0; dataIndex < reader.FieldCount; dataIndex++)
-                {
-                    data[dataIndex] = reader.GetValue(dataIndex);
-                }           
-                datastore.AddData(data);
-
-                if (StatusHelper.MustShowProgress(counter, -1) == true)
-                {
-                    reportProgress(new SimpleProgressReport("Loaded " + (counter + 1) + " records"));
-                }
-
-                counter++;
             }
-            sqlConnection.Dispose();
         }
 
         public void SetConfiguration(ConfigurationBase configurationBase)
