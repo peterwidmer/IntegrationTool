c93768529ac308a1df1aaf4b2ad9d6c8ec1d72a3
peterwidmer <peter.widmer@mail.com>
2016-07-23 23:08:43 +0200

    Improved loghandling of Write To Dynamics CRM target (logs now filterable)

--- IntegrationTool.Module.WriteToDynamicsCrm/Logging/RecordLogList/RecordLogListControl.xaml.cs
+++ IntegrationTool.Module.WriteToDynamicsCrm/Logging/RecordLogList/RecordLogListControl.xaml.cs
@@ -21,6 +21,8 @@ namespace IntegrationTool.Module.WriteToDynamicsCrm.Logging.RecordLogList
     /// </summary>
     public partial class RecordLogListControl : UserControl
     {
+        private ObservableCollection<RecordLog> recordLogs;
+
         public RecordLogListControl()
         {
             InitializeComponent();
@@ -28,7 +30,38 @@ namespace IntegrationTool.Module.WriteToDynamicsCrm.Logging.RecordLogList
 
         public void SetModel(ObservableCollection<RecordLog> recordLogs)
         {
-            this.DataContext = recordLogs;
+            this.recordLogs = recordLogs;
+            ApplyRecordFilter("all");
+        }
+
+        private void ddLogFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string filter = ((ComboBoxItem)ddLogFilter.SelectedItem).Tag.ToString();
+            ApplyRecordFilter(filter);
+        }
+
+        private void ApplyRecordFilter(string filter)
+        {
+            switch (filter)
+            {
+                case "successful":
+                    this.DataContext = recordLogs.Where(t => String.IsNullOrEmpty(t.WriteError));
+                    break;
+
+                case "errors":
+                    this.DataContext = recordLogs.Where(t => !String.IsNullOrEmpty(t.WriteError));
+                    break;
+
+                case "multiple_combined_key":
+                    this.DataContext = recordLogs
+                        .Where(recordLog => this.recordLogs.GroupBy(t => t.CombinedBusinessKey).Where(grp => grp.Count() > 1).Select(grp => grp.Key).Contains(recordLog.CombinedBusinessKey))
+                        .OrderBy(recordLog => recordLog.CombinedBusinessKey);
+                    break;
+
+                default:
+                    this.DataContext = recordLogs;
+                    break;
+            }
         }
     }
 }
