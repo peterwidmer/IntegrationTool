88bd13f7b8574e79d5118230401b707cd9066cdd
peterwidmer <peter.widmer@mail.com>
2016-10-09 21:54:09 +0200

    Started working on MSSQL Target

---
+++ IntegrationTool.Module.WriteToMSSQL/WriteToMSSQL.WriteData.cs
@@ -0,0 +1,17 @@
+﻿using IntegrationTool.SDK;
+using IntegrationTool.SDK.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTool.Module.WriteToMSSQL
+{
+    public partial class WriteToMSSQL
+    {
+        public void WriteData(IConnection connection, IDatabaseInterface databaseInterface, IDatastore dataObject, ReportProgressMethod reportProgress)
+        {
+        }
+    }
+}
---
+++ IntegrationTool.Module.WriteToMSSQL/WriteToMSSQL.cs
@@ -0,0 +1,33 @@
+﻿using IntegrationTool.SDK;
+using IntegrationTool.SDK.Database;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTool.Module.WriteToMSSQL
+{
+    [TargetModuleAttribute(Name = "WriteToMSSQL",
+                           DisplayName = "MSSQL",
+                           ModuleType = ModuleType.Target,
+                           GroupName = ModuleGroup.Target,
+                           ConnectionType = typeof(SqlConnection),
+                           ConfigurationType = typeof(WriteToMSSQLConfiguration))]
+    public partial class WriteToMSSQL : IModule, IDataTarget
+    {
+        public WriteToMSSQLConfiguration Configuration { get; set; }
+
+        public System.Windows.Controls.UserControl RenderConfigurationWindow(ConfigurationBase configuration, IDatastore dataObject)
+        {
+            ConfigurationWindow configurationWindow = new ConfigurationWindow((WriteToMSSQLConfiguration)configuration, dataObject);
+            return configurationWindow;
+        }
+
+        public void SetConfiguration(ConfigurationBase configurationBase)
+        {
+            this.Configuration = configurationBase as WriteToMSSQLConfiguration;
+        }     
+    }
+}
