cd8f85b3083d672ceb19dec250045961b2da78aa
peterwidmer <peter.widmer@mail.com>
2017-11-10 22:40:24 +0100

    Started working on SQLite source

---
+++ IntegrationTool.Module.LoadFromSQLite/LoadFromSQLite.cs
@@ -0,0 +1,43 @@
+﻿using IntegrationTool.SDK;
+using IntegrationTool.SDK.Database;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTool.Module.LoadFromSQLite
+{
+    [SourceModuleAttribute(Name = "LoadFromSQLite",
+                           DisplayName = "SQLite",
+                           ModuleType = ModuleType.Source,
+                           GroupName = ModuleGroup.Source,
+                           ConnectionType = typeof(SQLiteConnection),
+                           ConfigurationType = typeof(LoadFromSQLiteConfiguration))]
+    public class LoadFromSQLite : IModule, IDataSource
+    {
+        public LoadFromSQLiteConfiguration Configuration { get; set; }
+
+        public LoadFromSQLite()
+        {
+            Configuration = new LoadFromSQLiteConfiguration();
+        }
+
+        public void SetConfiguration(ConfigurationBase configurationBase)
+        {
+            this.Configuration = configurationBase as LoadFromSQLiteConfiguration;
+        }
+
+        public System.Windows.Controls.UserControl RenderConfigurationWindow(ConfigurationBase configurationBase, IDatastore dataObject)
+        {
+            ConfigurationWindow configurationWindow = new ConfigurationWindow((LoadFromSQLiteConfiguration)configurationBase);
+            return configurationWindow;
+        }
+
+        public void LoadData(IConnection connection, IDatastore datastore, ReportProgressMethod reportProgress)
+        {
+
+        }
+    }
+}
