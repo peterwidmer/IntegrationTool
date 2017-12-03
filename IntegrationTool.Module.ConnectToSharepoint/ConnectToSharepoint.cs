a143dafa738d1941dc1324dc7d6389ed24be2cf9
peterwidmer <peter.widmer@mail.com>
2017-12-03 11:14:21 +0100

    Started working on sharepoint integration

---
+++ IntegrationTool.Module.ConnectToSharepoint/ConnectToSharepoint.cs
@@ -0,0 +1,21 @@
+﻿using IntegrationTool.SDK;
+using IntegrationTool.SDK.Module.ModuleAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTool.Module.ConnectToSharepoint
+{
+    [ConnectionModuleAttribute(
+        DisplayName = "Sharepoint",
+        Name = "ConnectToSharepoint",
+        ContainsSubConfiguration = false,
+        ModuleType = ModuleType.Connection,
+        ConnectionType = typeof(string),
+        ConfigurationType = typeof(ConnectToSharepointConfiguration))]
+    public class ConnectToSharepoint
+    {
+    }
+}
---
+++ IntegrationTool.Module.ConnectToSharepoint/ConnectToSharepointConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using IntegrationTool.SDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTool.Module.ConnectToSharepoint
+{
+    public class ConnectToSharepointConfiguration : ConnectionConfigurationBase
+    {
+        public string SiteUrl { get; set; }
+    }
+}
